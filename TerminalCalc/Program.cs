1399c644e6311ab92140aebb25f569611e5bfed2
sastofficial <39431922+sastofficial@users.noreply.github.com>
2021-12-21 18:52:15 +0100

    Uploaded The sourcecode

---
+++ TerminalCalc/Program.cs
@@ -0,0 +1,19 @@
+﻿double num1;
+double num2;
+string calcmethod;
+double answer;
+Console.Write("Input a number: ");
+num1 = Convert.ToDouble(Console.ReadLine() );
+
+Console.Write ("Input another number: ");
+num2 = Convert.ToDouble( Console.ReadLine() );
+
+Console.Write ("What calculation method are you using? (+,-,/ or x) ");
+ calcmethod = Console.ReadLine();
+ 
+
+if (calcmethod = "+"){
+	 answer = num1 + num2;
+ Console.WriteLine(num1 + " + " + num2 + " = " + answer); } 
+	else{ Console.WriteLine("hello");
+ } 
